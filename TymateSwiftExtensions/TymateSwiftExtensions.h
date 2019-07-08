3ceab5d137d05fd756e565afe31019ba14a31baa
SimonL <simon@tymate.com>
2019-07-08 17:57:08 +0200

    init

---
+++ TymateSwiftExtensions/TymateSwiftExtensions.h
@@ -0,0 +1,19 @@
+//
+//  TymateSwiftExtensions.h
+//  TymateSwiftExtensions
+//
+//  Created by Simon on 08/07/2019.
+//  Copyright © 2019 Tymate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TymateSwiftExtensions.
+FOUNDATION_EXPORT double TymateSwiftExtensionsVersionNumber;
+
+//! Project version string for TymateSwiftExtensions.
+FOUNDATION_EXPORT const unsigned char TymateSwiftExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TymateSwiftExtensions/PublicHeader.h>
+
+
