6579b65dba1a6f1b1f825fb2f07edfcb393d5526
SimonL <simon@tymate.com>
2019-07-11 15:32:59 +0200

    add new version and some new extensions

--- TymateSwiftExtensions/UIImageViewExtensions.swift
+++ TymateSwiftExtensions/UIImageViewExtensions.swift
@@ -7,21 +7,21 @@
 //
 
 import Foundation
-import Kingfisher
+//import Kingfisher
 
 public extension UIImageView {
-    func loadImage(withUrl url: String?,
-                   andPlaceHolder placeHolder: UIImage? = nil, preloadWithPlaceHolder: Bool = true) {
-        if preloadWithPlaceHolder {
-            self.image = placeHolder ?? UIImage(named: "placeholder")
-        }
-        guard let urlString = url, let url = URL(string: urlString) else {
-            self.image = placeHolder ?? UIImage(named: "placeholder")
-            return
-        }
-        let resource = ImageResource(downloadURL: url, cacheKey: url.path)
-        self.kf.setImage(with: resource, placeholder: placeHolder)
-    }
+//    func loadImage(withUrl url: String?,
+//                   andPlaceHolder placeHolder: UIImage? = nil, preloadWithPlaceHolder: Bool = true) {
+//        if preloadWithPlaceHolder {
+//            self.image = placeHolder ?? UIImage(named: "placeholder")
+//        }
+//        guard let urlString = url, let url = URL(string: urlString) else {
+//            self.image = placeHolder ?? UIImage(named: "placeholder")
+//            return
+//        }
+//        let resource = ImageResource(downloadURL: url, cacheKey: url.path)
+//        self.kf.setImage(with: resource, placeholder: placeHolder)
+//    }
 
     func designRounded(radius: Int) {
         self.layer.cornerRadius = CGFloat(radius)
--- TymateSwiftExtensions/UIViewController.swift
+++ TymateSwiftExtensions/UIViewController.swift
@@ -8,18 +8,18 @@
 
 import Foundation
 import UIKit
-import Toast_Swift
+//import Toast_Swift
 
 extension UIViewController {
 
-    func showToast(title: String, subtitle: String? = nil, image: UIImage? = nil, duration: TimeInterval = 3, position: ToastPosition = .bottom, view: UIView? = nil) {
-        let optionnalShowingView = view ?? self.tabBarController?.view ?? self.view
-        guard let showingView = optionnalShowingView else {return}
-
-        if let subtitle = subtitle, subtitle.contains("offline") {
-            showingView.makeToast(NSLocalizedString("Pas de connexion internet. Vérifiez votre connexion et revenez sur la page.", comment: "Error internet connection"), duration: 8, position: position, title: title, image: UIImage.init(named: "icLostConnexion"), completion: nil)
-        } else {
-            showingView.makeToast(subtitle, duration: duration, position: position, title: title, image: image, completion: nil)
-        }
-    }
+//    func showToast(title: String, subtitle: String? = nil, image: UIImage? = nil, duration: TimeInterval = 3, position: ToastPosition = .bottom, view: UIView? = nil) {
+//        let optionnalShowingView = view ?? self.tabBarController?.view ?? self.view
+//        guard let showingView = optionnalShowingView else {return}
+//
+//        if let subtitle = subtitle, subtitle.contains("offline") {
+//            showingView.makeToast(NSLocalizedString("Pas de connexion internet. Vérifiez votre connexion et revenez sur la page.", comment: "Error internet connection"), duration: 8, position: position, title: title, image: UIImage.init(named: "icLostConnexion"), completion: nil)
+//        } else {
+//            showingView.makeToast(subtitle, duration: duration, position: position, title: title, image: image, completion: nil)
+//        }
+//    }
 }
