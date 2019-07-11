1a1615c0f880fbe270fca6d70b64c798eb25f4eb
SimonL <simon@tymate.com>
2019-07-11 15:22:57 +0200

    adding git ignore

---
+++ TymateSwiftExtensions/UIImageViewExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIImageViewExtensions.swift
+//  TymateSwiftExtensions
+//
+//  Created by Simon on 11/07/2019.
+//  Copyright © 2019 Tymate. All rights reserved.
+//
+
+import Foundation
---
+++ TymateSwiftExtensions/UIViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController.swift
+//  TymateSwiftExtensions
+//
+//  Created by Simon on 11/07/2019.
+//  Copyright © 2019 Tymate. All rights reserved.
+//
+
+import Foundation
